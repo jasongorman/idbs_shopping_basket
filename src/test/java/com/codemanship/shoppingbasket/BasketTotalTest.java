ed90bab5104d8bd8f10bbc525a79dc0ff2cafe45
Jason Gorman <jason.gorman@codemanship.com>
2025-05-27 14:20:45 +0100

    Added a test for total of an empty basket

---
+++ src/test/java/com/codemanship/shoppingbasket/BasketTotalTest.java
@@ -0,0 +1,13 @@
+package com.codemanship.shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasketTotalTest {
+    @Test
+    void totalOfEmptyBasket(){
+        Basket basket = new Basket();
+        assertEquals(basket.total(), 0.0, 0);
+    }
+}
