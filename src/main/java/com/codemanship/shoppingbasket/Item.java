f4ebd38990b0d06c2ef26fd7c7fa1e8f6edfb3e5
Jason Gorman <jason.gorman@codemanship.com>
2025-06-03 10:47:34 +0100

    Added test for total of two items

--- src/main/java/com/codemanship/shoppingbasket/Item.java
+++ src/main/java/com/codemanship/shoppingbasket/Item.java
@@ -1,6 +1,13 @@
 package com.codemanship.shoppingbasket;
 
 public class Item {
+    private double price;
+
     public Item(double price, int quantity) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }
