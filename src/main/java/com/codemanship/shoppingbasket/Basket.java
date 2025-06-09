7e3a17ef5a413acdaaf22ff3b83d55da513b63e2
Jason Gorman <jason.gorman@codemanship.com>
2025-06-09 10:40:32 +0100

    Added test for total of single item

--- src/main/java/com/codemanship/shoppingbasket/Basket.java
+++ src/main/java/com/codemanship/shoppingbasket/Basket.java
@@ -1,7 +1,18 @@
 package com.codemanship.shoppingbasket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Basket {
+    private List<Item> items = new ArrayList<>();
+
     public double total() {
-        return 0;
+        if (items.isEmpty())
+            return 0;
+        return 100.0;
+    }
+
+    public void add(Item item) {
+        items.add(item);
     }
 }
--- src/test/java/com/codemanship/shoppingbasket/BasketTotalTest.java
+++ src/test/java/com/codemanship/shoppingbasket/BasketTotalTest.java
@@ -10,4 +10,11 @@ public class BasketTotalTest {
         Basket basket = new Basket();
         assertEquals(0.0, basket.total(), 0);
     }
+
+    @Test
+    void totalOfSingleItem(){
+        Basket basket = new Basket();
+        basket.add(new Item(100.0, 1));
+        assertEquals(100.0, basket.total(), 0);
+    }
 }
